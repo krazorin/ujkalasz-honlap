3b413d22528b84ed232e9f182f7576b03fd36bfc
Krazorin <krazorin@email.com>
2024-09-26 23:38:36 +0100

    feat: progress

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from "react";
+import styles from "./Footer.module.scss";
+
+const Footer: FunctionComponent = () => {
+  return <div className={styles.container}>Cím</div>;
+};
+
+export default Footer;
---
+++ src/components/PageLayout/PageLayout.tsx
@@ -0,0 +1,22 @@
+import { FunctionComponent, ReactNode } from "react";
+
+import Header from "src/components/Header/Header";
+import Footer from "src/components/Footer/Footer";
+
+import styles from "./PageLayout.module.scss";
+
+interface Props {
+  children: ReactNode | ReactNode[];
+}
+
+const PageLayout: FunctionComponent<Props> = ({ children }) => {
+  return (
+    <div className={styles.pageLayoutContainer}>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default PageLayout;
