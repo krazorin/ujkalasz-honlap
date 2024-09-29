3c5d32e01a1a451599b8bcf86e6814eca5865858
Krazorin <krazorin@email.com>
2024-09-29 13:34:48 +0100

    feat: finish nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,9 +12,10 @@ interface NavItem {
 
 const desktopOtherLinkDropdownLabelkWidth = 150
 
+// Utility to calculate nav item width based on label length
 const navItemWidth = (label: string, withoutColumnGap?: boolean) => {
-  const charRenderWidth = 10.5 // Font size 20
-  const paddingWidth = 30 // Nav item padding width * 2
+  const charRenderWidth = 10.5
+  const paddingWidth = 30
   const desktopNavColumnGap = 20
 
   return withoutColumnGap
@@ -22,9 +23,8 @@ const navItemWidth = (label: string, withoutColumnGap?: boolean) => {
     : label.length * charRenderWidth + paddingWidth + desktopNavColumnGap
 }
 
-const isLastItem = (array: unknown[], index: number) => {
-  return array.length - 1 === index
-}
+const isLastItem = (array: unknown[], index: number) =>
+  array.length - 1 === index
 
 const navItems: NavItem[] = [
   { id: 1, label: "Hírek", link: "/" },
@@ -60,43 +60,38 @@ const navItems: NavItem[] = [
   { id: 9, label: "Elérhetőségeink", link: "/elerhetosegeink" },
 ]
 
-const createDesktopNavLink = (
-  itemId: number,
+// Helper function for creating NavLinks with dynamic classNames
+const createNavLink = (
+  id: number,
   label: string,
-  path: string,
-  isSubItem?: boolean,
+  link: string,
+  isSubItem = false,
+  isDesktop = true,
 ) => {
+  const getClassNames = (isActive: boolean) => {
+    const classNames = []
+    if (isSubItem) {
+      classNames.push(
+        isDesktop ? styles.desktopNavSubLink : styles.mobileSubLink,
+      )
+    } else {
+      classNames.push(isDesktop ? styles.desktopNavLink : styles.mobileNavLink)
+    }
+
+    if (isActive) {
+      classNames.push(
+        isDesktop ? styles.activeDesktopNavLink : styles.activeMobileLink,
+      )
+    }
+
+    return classNames.join(" ")
+  }
+
   return (
     <NavLink
-      key={`nav-item-${itemId}`}
-      id={`nav-item-${itemId}`}
-      to={path}
-      className={({ isActive }) => {
-        const classNames: string[] = []
-
-        if (isSubItem) {
-          classNames.push(styles.desktopNavSubLink)
-        } else {
-          classNames.push(styles.desktopNavLink)
-        }
-
-        if (isActive) {
-          classNames.push(styles.activeDesktopNavLink)
-        }
-
-        return classNames.join(" ")
-      }}
-    >
-      {label}
-    </NavLink>
-  )
-}
-
-const createMobileNavLink = (label: string, path: string) => {
-  return (
-    <NavLink
-      to={path}
-      className={({ isActive }) => (isActive ? styles.activeMobileLink : "")}
+      key={`nav-item-${id}`}
+      to={link}
+      className={({ isActive }) => getClassNames(isActive)}
     >
       {label}
     </NavLink>
@@ -106,11 +101,10 @@ const createMobileNavLink = (label: string, path: string) => {
 const Header: FunctionComponent = () => {
   const [visibleItems, setVisibleItems] = useState<NavItem[]>(navItems)
   const [overflowItems, setOverflowItems] = useState<NavItem[]>([])
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
   const containerRef = useRef<HTMLDivElement>(null)
 
-  // State for handling the mobile menu toggle
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
-
+  // Adjust visible and overflow items based on container width
   useEffect(() => {
     const handleResize = () => {
       if (containerRef.current) {
@@ -119,32 +113,25 @@ const Header: FunctionComponent = () => {
         const visible: NavItem[] = []
         const overflow: NavItem[] = []
 
-        for (let i = 0; i < navItems.length; i++) {
-          let itemWidth = navItemWidth(navItems[i].label)
-
-          if (isLastItem(navItems, i)) {
-            itemWidth = navItemWidth(navItems[i].label, true)
-          }
-
+        navItems.forEach((item, index) => {
+          const itemWidth = navItemWidth(
+            item.label,
+            isLastItem(navItems, index),
+          )
           totalWidth += itemWidth
 
-          if (isLastItem(navItems, i)) {
-            if (totalWidth < containerWidth) {
-              visible.push(navItems[i])
-            } else {
-              overflow.push(navItems[i])
-            }
+          if (
+            totalWidth +
+              (isLastItem(navItems, index)
+                ? 0
+                : desktopOtherLinkDropdownLabelkWidth) <
+            containerWidth
+          ) {
+            visible.push(item)
           } else {
-            if (
-              totalWidth + desktopOtherLinkDropdownLabelkWidth <
-              containerWidth
-            ) {
-              visible.push(navItems[i])
-            } else {
-              overflow.push(navItems[i])
-            }
+            overflow.push(item)
           }
-        }
+        })
 
         setVisibleItems(visible)
         setOverflowItems(overflow)
@@ -157,51 +144,43 @@ const Header: FunctionComponent = () => {
     return () => window.removeEventListener("resize", handleResize)
   }, [])
 
-  const renderNavItemWithDropdown = (item: NavItem) => {
-    return (
-      <div className={styles.navDropdown} key={item.id}>
-        {createDesktopNavLink(item.id, item.label, item.link)}
-        {item.subItems && (
-          <div className={styles.navDropdownContent}>
-            {item.subItems.map((subItem) =>
-              createDesktopNavLink(
-                subItem.id,
-                subItem.label,
-                subItem.link,
-                true,
-              ),
-            )}
-          </div>
-        )}
-      </div>
-    )
-  }
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen)
-  }
+  const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen)
 
   return (
     <div className={styles.container}>
       <nav className={styles.navigation}>
         <div className={styles.navigationLogoContainer}>
-          <img src={`${kalaszLogo}`} alt="Kalász logo" />
+          <img src={kalaszLogo} alt="Kalász logo" />
           <span>KALÁSZ</span>
         </div>
 
         {/* Desktop Navigation */}
         <div className={styles.desktopNavigation} ref={containerRef}>
           {visibleItems.map((item) =>
-            item.subItems
-              ? renderNavItemWithDropdown(item)
-              : createDesktopNavLink(item.id, item.label, item.link),
+            item.subItems ? (
+              <div className={styles.navDropdown} key={item.id}>
+                {createNavLink(item.id, item.label, item.link)}
+                <div className={styles.navDropdownContent}>
+                  {item.subItems.map((subItem) =>
+                    createNavLink(
+                      subItem.id,
+                      subItem.label,
+                      subItem.link,
+                      true,
+                    ),
+                  )}
+                </div>
+              </div>
+            ) : (
+              createNavLink(item.id, item.label, item.link)
+            ),
           )}
           {overflowItems.length > 0 && (
             <div className={styles.navDropdown}>
-              {createDesktopNavLink(100, "továbbiak", "")}
+              {createNavLink(100, "továbbiak", "")}
               <div className={styles.navDropdownContent}>
                 {overflowItems.map((item) =>
-                  createDesktopNavLink(item.id, item.label, item.link, true),
+                  createNavLink(item.id, item.label, item.link, true),
                 )}
               </div>
             </div>
@@ -210,7 +189,6 @@ const Header: FunctionComponent = () => {
 
         {/* Mobile Navigation */}
         <div className={styles.mobileNavigation}>
-          {/* Burger Icon for Mobile Menu */}
           <div className={styles.burgerIcon} onClick={toggleMobileMenu}>
             <span className={isMobileMenuOpen ? styles.open : ""}>☰</span>
           </div>
@@ -219,14 +197,18 @@ const Header: FunctionComponent = () => {
             <ul className={styles.mobileMenu}>
               {navItems.map((item) => (
                 <li key={item.id}>
-                  {createMobileNavLink(item.label, item.link)}
+                  {createNavLink(item.id, item.label, item.link, false, false)}
                   {item.subItems && (
                     <ul className={styles.mobileSubMenu}>
-                      {item.subItems.map((subItem) => (
-                        <li key={subItem.id}>
-                          {createMobileNavLink(subItem.label, subItem.link)}
-                        </li>
-                      ))}
+                      {item.subItems.map((subItem) =>
+                        createNavLink(
+                          subItem.id,
+                          subItem.label,
+                          subItem.link,
+                          true,
+                          false,
+                        ),
+                      )}
                     </ul>
                   )}
                 </li>
