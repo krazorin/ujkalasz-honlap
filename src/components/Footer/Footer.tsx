01aafa8518d9beff5188f68cdbda107791fc7d8c
Krazorin <krazorin@email.com>
2024-09-30 02:15:26 +0100

    feat: work on footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -19,18 +19,155 @@ const Footer: FunctionComponent = () => {
           <a
             href="https://www.google.com/maps/place/Budapest,+Hajn%C3%B3czy+J%C3%B3zsef+u.+10,+1122+Hungary/"
             target="_blank"
+            rel="noopener noreferrer"
           >
             1122 Budapest Hajnóczy utca 10. I/9.
           </a>
         </p>
       </div>
-      <div>
-        <h5>Támogatóink</h5>
+      <div className={styles.supporters}>
+        <p className={styles.title}>Támogatóink</p>
         <ul>
-          <li>Nemzeti</li>
-          <li>II kerulet</li>
-          <li>Hegyvidek</li>
-          <li>csaladbarat</li>
+          <li>
+            <a
+              href="https://kormany.hu/miniszterelnokseg"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className={styles.withBackground}
+                src="/assets/images/supporters/miniszterelnokseg.png"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://bgazrt.hu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className={styles.withBackground}
+                src="/assets/images/supporters/bethlen_gabor_alapkezelo.png"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://civil.info.hu/nea/kezdolap/bemutatkozas/index.html"
+              target="_blank"
+            >
+              <img src="/assets/images/supporters/NEA_2813.png" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://csalad.hu/rolunk/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                alt="CSBO-logo_szines.jpg"
+                src="/assets/images/supporters/CSBO-logo_szines.jpg"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://katolikus.hu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/assets/images/supporters/magyar_katolikus_puspokkari_konferencia.jpg" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.wucwo.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/assets/images/supporters/wucwo.png" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.nepfolakitelek.hu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/assets/images/supporters/lakitelek_nepfoiskola.jpg" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://www.szentkiraly.hu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className={styles.withBackground}
+                src="/assets/images/supporters/szentkiraly_kozseg.jpg"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://kapolnasnyek.hu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className={styles.withBackground}
+                src="/assets/images/supporters/kapolnasnyek.png"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://www.masodikkerulet.hu/fooldal"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                alt="budapest_2_ker.bmp"
+                src="/assets/images/supporters/budapest_2_ker.bmp"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.hegyvidek.hu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                alt="budapest_12_ker.bmp"
+                src="/assets/images/supporters/budapest_12_ker.bmp"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.varosmajoriplebania.hu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className={styles.withBackground}
+                src="/assets/images/supporters/varosmajor.bmp"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://orszagutiferencesek.hu/elerhetoseg-kapcsolat/filiak/krisztus-kiraly-filia/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src="/assets/images/supporters/krisztus_kiraly_templomigazgatosag.bmp" />
+            </a>
+          </li>
         </ul>
       </div>
     </div>
