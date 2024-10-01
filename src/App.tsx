d6e0669d1a69035df2abc9d41beab773e5d52c0e
Krazorin <krazorin@email.com>
2024-10-01 03:01:45 +0100

    feat: work on introduction pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import Home from "src/pages/Home/Home"
 import Bemutatkozas from "./pages/Bemutatkozas/Bemutatkozas"
 import Tortenetunk from "./pages/Bemutatkozas/Tortenetunk"
 import Vezerkonyv from "./pages/Bemutatkozas/Vezerkonyv"
+import Alapszabaly from "./pages/Bemutatkozas/Alapszabaly"
 
 import "src/App.css"
+import Kapcsolataink from "./pages/Bemutatkozas/Kapcsolataink"
 
 function App() {
   return (
@@ -16,7 +18,10 @@ function App() {
         <Route path="/tovabbiak" element={<Home />} />
         <Route path="/bemutatkozas" element={<Bemutatkozas />} />
         <Route path="/bemutatkozas/tortenetunk" element={<Tortenetunk />} />
-        <Route path="/bemutatkozas/Vezerkonyv" element={<Vezerkonyv />} />
+        <Route path="/bemutatkozas/vezerkonyv" element={<Vezerkonyv />} />
+        <Route path="/bemutatkozas/alapszabaly" element={<Alapszabaly />} />
+        <Route path="/bemutatkozas/kapcsolataink" element={<Kapcsolataink />} />
+
         <Route path="*" element={<Home />} />
       </Routes>
     </>
